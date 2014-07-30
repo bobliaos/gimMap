6d456f0b94b3dc2a7ad6f5d921e0ef99e288a8d0
gambabenton <gambabenton@gmail.com>
2014-07-31 00:09:08 +0800

    move some files

--- js/map/core/GimAStarGraph.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author bob / http://bobliaos.diandian.com
- * */
-
-GIM.GimAStarGraph = function (aStarNodes) 
-{
-	this.aStarNodes = aStarNodes;
-}
--- js/map/core/GimAStarNode.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author bob / http://bobliaos.diandian.com
- * */
-
-GIM.GimAStarNode = function () 
-{
-	this.id = undefined;
-}
