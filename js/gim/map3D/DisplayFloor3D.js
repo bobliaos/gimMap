f58736f7c71edac45a12f4ed48b9b25f346c73f4
bob <bobliaos@gmail.com>
2014-08-07 00:46:04 +0800

    new format of sgxml

--- js/gim/map3D/DisplayFloor3D.js
+++ js/gim/map3D/DisplayFloor3D.js
@@ -11,10 +11,12 @@ GIM.DisplayFloor3D = function(gElement){
 
     for(var key in this.data.unitsData){
         var unitData = this.data.unitsData[key];
-        var displayUnit3D = new GIM.DisplayUnit3D(unitData);
-        this.unit3DsContainer.add(displayUnit3D.mesh);
-        displayUnit3D.mesh.position.z = unitData.meshZ;
-        this.displayUnit3Ds[unitData.nodeId] = displayUnit3D;
+        if(unitData.nodeTypeId != "0"){
+            var displayUnit3D = new GIM.DisplayUnit3D(unitData);
+            this.unit3DsContainer.add(displayUnit3D.mesh);
+            displayUnit3D.mesh.position.z = unitData.meshZ;
+            this.displayUnit3Ds[unitData.nodeId] = displayUnit3D;
+        }
     }
     return this;
 }
